1bc554708426d28a7420918cf4699bd3b01cff45
eparadis <eparadis@users.noreply.github.com>
2020-11-22 07:25:20 +0000

    comments in input file; bug fixes

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
+#include <string.h>
 
 #define MAXRAM 300
 
@@ -47,6 +48,7 @@ void loadRamFromFile() {
   // from: https://www.tutorialspoint.com/c_standard_library/c_function_fgets.htm
   FILE *fp;
   char str[60];
+  char *p_str;
   int index;
 
   fp = fopen("file.txt", "r");
@@ -59,7 +61,8 @@ void loadRamFromFile() {
   index = 0;
   while( index < MAXRAM ) {
     if( fgets( str, 60, fp) != NULL) {
-      ram[index] = atoi( str);
+      p_str = str;
+      ram[index] = atoi( strsep(&p_str, " \t\r\n"));
     }
     index += 1;
   }
@@ -94,21 +97,18 @@ int main( int argc, char *argv[]) {
   do {
     cmd = getNextByte();
     switch( cmd) {
-      case 0x00:
-        // halt naturally
+      case 0x00: // ( -- ) halt
         retCode = 0;
         break;
-      case 0x01:
-        // read char from input and put on stack
+      case 0x01: // ( -- char ) read char from input and put on stack
         ch = getchar();
         push(ch);
         break;
-      case 0x02:
-        // pop char from stack and put into output
+      case 0x02: // ( char -- ) pop char and put into output
         ch = pop();
         putchar(ch);
         break;
-      case 0x03:
+      case 0x03: // ( c b a -- B ) B=b-a, IP=c if B<0
         // subtract and branch if negative. hey now we're turing complete right?
         // -- no, b/c this isn't how you impl SBN ;)
         a = pop();
@@ -120,44 +120,45 @@ int main( int argc, char *argv[]) {
           setIP(c);
         }
         break;
-      case 0x04:
-        // store a value in memory
+      case 0x04: // ( val loc -- ) store a value in memory
         a = pop(); // location
         b = pop(); // value
         ram[a] = b;
+        //printf("store %d into %d\n", b, a); 
         break;
-      case 0x05:
-        // read value from memory
+      case 0x05: // ( loc -- val ) read value from memory
         a = pop(); // location
         push( ram[a]);
+        //printf("load %d\n", a);
         break;
-      case 0x06:
-        // subtract
+      case 0x06: // ( b a -- c) c=b-a subtract
         a = pop();
         b = pop();
-        push( a-b);
+        push( b-a);
+        //printf( "subtract %d - %d \n", b, a);
         break;
-      case 0x07:
-        // add
+      case 0x07: // ( b a -- c) c=a+b add
         a = pop();
         b = pop();
         push( a+b);
         break;
-      case 0x08:
-        // if TOS is negative, set IP to the next value in the stack
+      case 0x08: // ( loc cond -- ) IP=loc if cond>0
+        // if TOS is positive, set IP to the next value in the stack
         a = pop(); // condition
         b = pop(); // location
-        if( a < 0) {
+        if( a > 0) {
           IP = b;
+          //printf("%d is greater than 0; jumping to %d\n", a, b);
+        } else {
+          //printf("%d is <= 0", a);
         }
         break;
-      case 0x09:
-        // immediate value - push the location after where IP is pointing on to the stack
+      case 0x09: // ( -- x ) immediate value - push the location after where IP is pointing on to the stack
         IP += 1; // so this is a two-location operand
         push( ram[IP]);
         break;
       default:
-        printf("unrecognized opcode %d\n", cmd);
+        printf("unrecognized opcode %d at %d\n", cmd, IP);
         retCode = -1;
         cmd = 0;
     }
