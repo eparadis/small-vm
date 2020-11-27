45805134852c4bf5c4a43bb5c77e900e38511bad
eparadis <eparadis@users.noreply.github.com>
2020-11-27 08:14:50 +0000

    no label support but everything else works

--- asmblr.c
+++ asmblr.c
@@ -4,6 +4,12 @@
 
 #define LINE_MAX 255
 
+void addLabel( char *label, int loc) {
+}
+
+void addAddress( char *label, int loc) {
+}
+
 void parseFile() {
   FILE *fp;
   char line[LINE_MAX];
@@ -16,6 +22,7 @@ void parseFile() {
     "jlz"
   };
   int i;
+  int location;
 
   fp = fopen("asm.txt", "r");
   if( fp == NULL) {
@@ -23,6 +30,7 @@ void parseFile() {
     return;
   }
 
+  location = 0;
   while( fgets( line, LINE_MAX, fp) != NULL) {
     // printf("%d: ", line_number);
     
@@ -32,32 +40,39 @@ void parseFile() {
     while( token != NULL && strcmp(token, "#")) {
       if( strncmp(token, ":", 1) == 0) {
         // label
-        printf("label: %s ", token);
+        // printf("label: %s ", token);
+        addLabel( token, location);
       } else if( strncmp( token, "@", 1) == 0) {
         // address reference
-        printf("address: %s ", token);
-      } else if( strpbrk( token, "0123456789") == token) {
-        // the first character is a digit
+        // printf("address: %s ", token);
+        addAddress(token, location);
+        location += 1;
+      } else if( strpbrk( token, "-0123456789") == token) {
+        // the first character is a digit or negative sign
         printf("%s ", token);
+        location += 1;
       } else if ( strcmp( token, "push") == 0) {
         // currently the only multibyte opcode
         printf("09 ");
+        location += 1;
       } else {
         //printf("word: %s ", token);
+        // the position in the array is the value of the opcode
         for( i=0; i<9; i+=1) {
           if( strcmp(token, words[i]) == 0) {
             printf("%d ", i);
             i=9999;
           }
         }
+        location += 1;
       }
       token = strtok(NULL, whitespace);
     }
   
-    //printf("\n");
     line_number += 1;
   }
 
+  printf("\n");
   fclose(fp);
 }
 
