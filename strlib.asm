2031a8111f0a87881d1e62b24ab881e113bfb585
Ed Paradis <eparadis@users.noreply.github.com>
2021-01-12 08:47:31 -0800

    WIP plan out more impls

--- strlib.asm
+++ strlib.asm
@@ -2,10 +2,18 @@
 
 .addmacro call # ( addr -- ) pushes an address to return to on to the control stack and jumps to the address addr
 # TODO call impl
+# increment stack pointer
+# push current IP
+# add an offset to account for the following code
+# copy it to where the stack pointer points
+# jump to addr
 .endmacro
 
 .addmacro return # ( -- ) pops the control stack and jumps to it
 # TODO return impl
+# push whatever the stack pointer points to
+# decrement the stack pointer
+# jump to what we pushed earlier
 .endmacro
 
 .addmacro puts # ( addr -- ) emits a null-terminated string that starts at addr
@@ -17,14 +25,20 @@ call
 .endmacro
 
 .addmacro putd # ( val -- ) emits val as a decimal
-# TODO putd impl
-# https://stackoverflow.com/questions/13166064/how-do-i-print-an-integer-in-assembly-level-programming-without-printf-from-the
+push @_strlib_putd
+call
 .endmacro
 
 :_strlib_top
-push @_strlib_start
+push @_strlib_init
 push 1
-jgz
+jgz # jump over all the variables and impls to the init
+
+# control stack
+:_strlib_cs
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+:_strlib_SP
+0
 
 # --- puts impl ---
 :_strlib_puts
@@ -67,14 +81,32 @@ jgz
 
 # continue at where we came from
 :_strlib_puts_end
-push @_strlib_puts_continue
-push 1
-jgz
+return
 
 # temporary variable for puts
 :_strlib_puts_index
 0
 
+# --- putd impl ---
+:_strlib_putd
+# discussion: https://stackoverflow.com/questions/13166064/how-do-i-print-an-integer-in-assembly-level-programming-without-printf-from-the
+# TODO putd impl
+# push a null
+# leftover = val
+# :top
+# push leftover mod 10
+# offset by '0'
+# leftover = leftover / 10
+# if leftover is > 10 goto top
+# while not null, pop and emit
+return
 
-:_strlib_start
-# init could go here
+# temp vars for putd
+:_strlib_putd_leftover
+0
+
+:_strlib_init
+# init the control stack
+push @_strlib_cs
+push @_strlib_SP
+store # store the address of the control stack into the stack pointer
