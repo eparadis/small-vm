a1f612dcad7f4f4738766bbee9432202fba79357
Ed Paradis <eparadis@users.noreply.github.com>
2021-01-14 13:48:15 -0800

    move strlib tests to tests dir

--- libtest.sh
+++ libtest.sh
@@ -4,7 +4,7 @@ export DIR='temp/libtest'
 
 mkdir -p $DIR
 
-cat strlib_macros.asm strlib_impl.asm strlib_test.asm > $DIR/one
+cat strlib_macros.asm strlib_impl.asm tests/strlib_test.asm > $DIR/one
 ./baz $DIR/one > $DIR/two
 cat strlib_macros.asm $DIR/two > $DIR/three
 ./baz $DIR/three > $DIR/four
