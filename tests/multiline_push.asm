69bf2777a94f83ffe318808fdac84961d0bc19cd
Ed Paradis <eparadis@users.noreply.github.com>
2021-01-14 09:21:56 -0800

    test multiline push statements b/c i was worried about them

---
+++ tests/multiline_push.asm
@@ -0,0 +1,18 @@
+# test of how i usually use push
+push 65
+emit
+
+# test of having multiline push statements
+push
+66
+emit
+
+# test of multiline push with comment
+
+push # comment comment beautiful comment
+67
+emit
+
+# new line
+push 10
+emit
