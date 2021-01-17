1d2786f5d9f20d262833b79daa1894e720072790
eparadis <eparadis@users.noreply.github.com>
2021-01-17 07:32:34 +0000

    name space labels for putd

--- putd.asm
+++ putd.asm
@@ -3,115 +3,118 @@
 # this will become a routine at some point
 # it will accept a single param and return nothing
 push 9  # > 4
+
+
+:_putd_impl
 # ( value -- )
-push @value
+push @_putd_value
 store
 
 # advance decr_idx and set decr to what it points to
-:next_decr
+:_putd_next_decr
 # increment the index
-push @decr_idx
+push @_putd_decr_idx
 read
 push 1
 add
-push @decr_idx
+push @_putd_decr_idx
 store
 
 # store the indexed value of the list into decr
-push @decr_list # > @decr_list
-push @decr_idx  # > @decr_list @decr_idx
+push @_putd_decr_list # > @decr_list
+push @_putd_decr_idx  # > @decr_list @decr_idx
 read            # > @decr_list 0
 add             # > @decr_list+0
 read            # > [0th item of decr list, 100]
-push @decr      # > 100 @decr
+push @_putd_decr      # > 100 @decr
 store           # > (empty) # and decr is 100
 
 # if decr gt 0, we've got more in our list
-push @go
-push @decr
+push @_putd_go
+push @_putd_decr
 read
 jgz
 # else we are done. jump to end
-push @end
+push @_putd_end
 push 1
 jgz
 
-:go
+:_putd_go
 # init the character
 push 47 # > 47     # 48 is '0' 
-push @char # > 47 @char
+push @_putd_char # > 47 @char
 store   # > (empty)   # and char is 47
 
-:top
+:_putd_top
 # increment the character
-push @char # > @char
+push @_putd_char # > @char
 read       # > 47
 push 1     # > 47 1
 add        # > 48
-push @char # > 48 @char
+push @_putd_char # > 48 @char
 store      # > (empty)    # and char is 48
 
 # decrement through our value
-push @value
+push @_putd_value
 read       # > 4
-push @decr
+push @_putd_decr
 read
 sub        # > 3
-push @value # > 3 @value
+push @_putd_value # > 3 @value
 store      # > (empty) # and value is 3
 
 # we now need to jump to top if value is gte 0
 # jump if value is gt 0
-push @top  # > @top
-push @value # > @top @value
+push @_putd_top  # > @top
+push @_putd_value # > @top @value
 read       # > @top value
 jgz        # > (empty)
 # value is either 0 or negative
-push @top
-push @value
+push @_putd_top
+push @_putd_value
 read
 push 1
 add   # if value was 0, then TOS is 1, else its 0 or below
 jgz
 
 # "undo" the last subtract of decr so value can be used again
-push @decr
+push @_putd_decr
 read
-push @value
+push @_putd_value
 read
 add
-push @value
+push @_putd_value
 store
 
 # emit the counter
-:do_emit
-push @char
+:_putd_do_emit
+push @_putd_char
 read
 emit
 
 # if decr is gt 0, jump to get next decr
-push @next_decr
-push @decr
+push @_putd_next_decr
+push @_putd_decr
 read
 jgz
 
-:end
+:_putd_end
 
 push 10 # newline
 emit
 halt
 
-:value # we slowly subtract out powers of the radix until this is 0
+:_putd_value # we slowly subtract out powers of the radix until this is 0
 0
 
-:char # the current character to print
+:_putd_char # the current character to print
 0
 
-:decr # the current radix power we're decrementing by
+:_putd_decr # the current radix power we're decrementing by
 0
 
-:decr_list # the powers of the radix we're using, zero terminated
+:_putd_decr_list # the powers of the radix we're using, zero terminated
 100 10 1 0
 
-:decr_idx # index to above list.
+:_putd_decr_idx # index to above list.
 -1
